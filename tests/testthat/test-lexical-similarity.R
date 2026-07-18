550ef05758f199e89cea6f67dd12a189aaf24508
Henry-Osorto <henryantonioosortoruiz@gmail.com>
2026-07-18 08:52:11 -0600

    Update test_that

--- tests/testthat/test-lexical-similarity.R
+++ tests/testthat/test-lexical-similarity.R
@@ -1,23 +1,35 @@
 
 test_that("TF-IDF, BM25 and exact scores remain between zero and one", {
   directory <- withr::local_tempdir()
-  release <- build_database_release(
-    create_test_articles(),
-    data_version = "2026.07",
-    output_dir = directory
-  )
+  
+  release <- build_database_release(create_test_articles(),
+                                    data_version = "2026.07",
+                                    output_dir = directory)
+  
   withr::local_options(list(similR.cache_dir = withr::local_tempdir()))
+  
   index <- similR:::build_lexical_index(release$database_path, use_cache = FALSE)
+  
   dimension <- index$dimensions$theme
+  
   query <- "artificial intelligence entrepreneurship university students"
-  scores <- list(
-    tfidf = similR:::tfidf_similarity(query, dimension),
-    bm25 = similR:::bm25_similarity(query, dimension),
-    exact = similR:::exact_match_score(query, dimension)
-  )
+  
+  scores <- list(tfidf = similR:::tfidf_similarity(query, dimension),
+                 bm25 = similR:::bm25_similarity(query, dimension),
+                 exact = similR:::exact_match_score(query, dimension))
+  
+  expect_length(scores$tfidf,
+                nrow(dimension$tfidf))
+  
+  expect_false(anyNA(scores$tfidf))
+  
+  expect_true(all(is.finite(scores$tfidf)))
+  
   expect_true(all(vapply(scores, function(x) all(x >= 0 & x <= 1), logical(1))))
+  
   expect_gt(scores$tfidf[[1L]], scores$tfidf[[2L]])
   expect_gt(scores$bm25[[1L]], scores$bm25[[2L]])
+  
 })
 
 test_that("cosine similarity handles regular and zero vectors", {
